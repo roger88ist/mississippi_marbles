6afd2cbc87dd2dd0786d3d59f3266a18939dd534
Rogerio Istiraneopulos <roger88ist@gmail.com>
2017-09-10 00:07:38 -0400

    add roll logic

--- main.rb
+++ main.rb
@@ -14,28 +14,39 @@ num_players.times do |i|
 end
 
 loop do
-  system 'clear'
-
-  # show scoreboard
-  players.each do |player|
-    puts "#{player.name}: \t#{player.points} |  (#{11000 - player.points})"
-  end
-  puts "\n"
 
   players.each do |player|
+    system 'clear'
+
+    # show scoreboard
+    players.each do |player|
+      puts "#{player.name}: \t#{player.points} |  (#{11000 - player.points})"
+    end
+    puts "\n"
+
     roll_num = 1
     keep_going = true
 
     puts "#{player.name}'s Turn"
+
+    turn_score = 0
     while keep_going
+
       print "Roll #{roll_num}: "
       roll_value = gets.chomp
+
       if roll_value == "0"
         keep_going = false
+      elsif roll_value.downcase == "wipe" || roll_value.downcase == "w"
+        keep_going = false
+        player.points = 0
+      elsif roll_value.downcase == "keep" || roll_value.downcase == "k"
+        keep_going = false
+        player.points += turn_score
+      else
+        turn_score += roll_value.to_i
+        roll_num += 1
       end
-      roll_num += 1
     end
-    
   end
-  
 end
