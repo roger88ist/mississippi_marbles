e2a8cf8e3611751b222e3d8e014647a621f04025
Graham Sutton <grahamsutton2@gmail.com>
2017-09-10 00:17:54 -0400

    Updated the Hand class.

--- hand.rb
+++ hand.rb
@@ -1,9 +1,26 @@
 class Hand
 
-  # Dice kept in the current hand.
+  # Constructor
   #
-  # @var Array
-  kept = []
+  # Creates 6 dice to be rolled for the current hand.
+  def initialize
+    6.times do
+      self.dice << Die.new
+    end
+  end
+
+  # Rolls all dice currently not kept.
+  #
+  # @return void
+  def roll
+    results = []
+
+    self.dice.each do |die|
+      results << die.roll
+    end
+
+    return results
+  end
 
   # Keeps a die.
   #
